b6bf9d2ce953046b66733cfb6456ac40658e3f76
cristianschiaroli <cristianschiaroli@192.168.1.139>
2019-06-10 17:43:03 +0200

    Terzo commit
    Scrittura javadoc su parte delle classi

--- progetto_OOP/src/progetto_OOP/Ente.java
+++ progetto_OOP/src/progetto_OOP/Ente.java
@@ -1,6 +1,17 @@
 package progetto_OOP;
 
+/**
+ * 
+ * Classe Ente derivata dalla classe Info contenente informazioni su un ente
+ *
+ */
 public class Ente extends Info {
+	
+	/**
+	 * Costruttore della classe ente
+	 * @param codice codice ente
+	 * @param nome nome ente
+	 */
 	public Ente(String codice, String nome) {
 		super(codice, nome);
 	}
--- progetto_OOP/src/progetto_OOP/Spesa.java
+++ progetto_OOP/src/progetto_OOP/Spesa.java
@@ -1,10 +1,22 @@
 package progetto_OOP;
 
+/**
+ * 
+ * Classe Spesa derivata dalla classe Info che contiene informazioni sulle spese effettuate dai vari enti regionali in un anno
+ *
+ */
 public class Spesa extends Info {
 	private int spesa_laurea;
 	private int spesa_dottorato;
 	private int spesa_specializzazione;
-	
+	/**
+	 * Costruttore della classe Spesa
+	 * @param codice Codice Spesa
+	 * @param nome Descrizione della  Spesa
+	 * @param spesa_laurea Spesa per laurea
+	 * @param spesa_dottorato Spesa per dottorato 
+	 * @param spesa_specializzazione Spesa per specializzzzione
+	 */
 	public Spesa(String codice, String nome, int spesa_laurea, int spesa_dottorato, int spesa_specializzazione) {
 		super(codice, nome);
 		this.spesa_laurea = spesa_laurea;
