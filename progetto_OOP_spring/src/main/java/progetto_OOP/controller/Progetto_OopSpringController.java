dc2b3b7f32f2b68e20a694f3546ad84d1d997f24
giovanni <giovanni@192.168.1.108>
2019-06-17 17:31:49 +0200

    nono commit piccola correzione packages

--- progetto_OOP_spring/src/main/java/progetto_OOP/controller/Progetto_OopSpringController.java
+++ progetto_OOP_spring/src/main/java/progetto_OOP/controller/Progetto_OopSpringController.java
@@ -1,4 +1,4 @@
-package progetto_OOP_spring.controller;
+package progetto_OOP.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
