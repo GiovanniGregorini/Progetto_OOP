e9ee05a47f0e7c737f5141a1a78d39782134b0d5
cristianschiaroli <cristianschiaroli@192.168.0.102>
2019-06-19 19:45:56 +0200

    Dodicesimo commit
    implementazione classi ContaOccorrenze e GetStats

--- progetto_OOP_spring/src/main/java/progetto_OOP/classes/ContaOccorrenze.java
+++ progetto_OOP_spring/src/main/java/progetto_OOP/classes/ContaOccorrenze.java
@@ -6,13 +6,93 @@ import java.util.HashMap;
 import java.util.Map; 
 
 public class ContaOccorrenze { 
- public static Map<String, Integer> contaOccorrenze(BancaDati dati) { 
+ public static Map<String, Integer> contaOccorrenzeNomeRegione(BancaDati dati) { 
      // hashmap to store the frequency of element 
      Map<String, Integer> hm = new HashMap<String, Integer>(); 
 
      for(Dati elem : dati.getDati()) { 
          Integer j = hm.get(elem.getDati_regione().getNome()); 
          hm.put(elem.getDati_regione().getNome(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeCodiceRegione(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_regione().getCodice()); 
+         hm.put(elem.getDati_regione().getCodice(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeNomeEnte(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_ente().getNome()); 
+         hm.put(elem.getDati_ente().getNome(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeCodiceEnte(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_ente().getCodice()); 
+         hm.put(elem.getDati_ente().getCodice(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeNomeIstituto(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_istituto().getNome()); 
+         hm.put(elem.getDati_istituto().getNome(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeCodiceIstituto(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_istituto().getCodice()); 
+         hm.put(elem.getDati_istituto().getCodice(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeTipoIstituto(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_istituto().getTipo_istituto()); 
+         hm.put(elem.getDati_istituto().getTipo_istituto(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeNomeSpesa(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_spesa().getNome()); 
+         hm.put(elem.getDati_spesa().getNome(), (j == null) ? 1 : j + 1); 
+     }
+     return hm;
+ }
+ public static Map<String, Integer> contaOccorrenzeCodiceSpesa(BancaDati dati) { 
+     // hashmap to store the frequency of element 
+     Map<String, Integer> hm = new HashMap<String, Integer>(); 
+
+     for(Dati elem : dati.getDati()) { 
+         Integer j = hm.get(elem.getDati_spesa().getCodice()); 
+         hm.put(elem.getDati_spesa().getCodice(), (j == null) ? 1 : j + 1); 
      }
      return hm;
  }
--- progetto_OOP_spring/src/main/java/progetto_OOP/classes/GetStats.java
+++ progetto_OOP_spring/src/main/java/progetto_OOP/classes/GetStats.java
@@ -8,7 +8,15 @@ public class GetStats {
 		stats.add(new StatsNum("spesa_laurea",dati.avgspesa_laurea(), dati.minspesa_laurea(), dati.maxspesa_laurea(), dati.stdspesa_laurea(),dati.sumspesa_laurea(), dati.count()));
 		stats.add(new StatsNum("spesa_dottorato",dati.avgspesa_dottorato(), dati.minspesa_dottorato(), dati.maxspesa_dottorato(), dati.stdspesa_laurea(),dati.sumspesa_dottorato(), dati.count()));
 		stats.add(new StatsNum("spesa_specializzazione",dati.avgspesa_specializzazione(), dati.minspesa_specializzazione(), dati.maxspesa_specializzazione(), dati.stdspesa_specializzazione(),dati.sumspesa_specializzazione(), dati.count()));
-		stats.add(new StatsStr("Regione", ContaOccorrenze.contaOccorrenze(dati)));
+		stats.add(new StatsStr("Nome Regione", ContaOccorrenze.contaOccorrenzeNomeRegione(dati)));
+		stats.add(new StatsStr("Codice Regione", ContaOccorrenze.contaOccorrenzeCodiceRegione(dati)));
+		stats.add(new StatsStr("Nome Ente", ContaOccorrenze.contaOccorrenzeNomeEnte(dati)));
+		stats.add(new StatsStr("Codice Ente", ContaOccorrenze.contaOccorrenzeCodiceEnte(dati)));
+		stats.add(new StatsStr("Nome Istituto", ContaOccorrenze.contaOccorrenzeNomeIstituto(dati)));
+		stats.add(new StatsStr("Codice Istituto", ContaOccorrenze.contaOccorrenzeCodiceIstituto(dati)));
+		stats.add(new StatsStr("Tipo Istituto", ContaOccorrenze.contaOccorrenzeTipoIstituto(dati)));
+		stats.add(new StatsStr("Nome Spesa", ContaOccorrenze.contaOccorrenzeNomeSpesa(dati)));
+		stats.add(new StatsStr("Codice Spesa", ContaOccorrenze.contaOccorrenzeCodiceSpesa(dati)));
 	return stats;
 	}
 }
